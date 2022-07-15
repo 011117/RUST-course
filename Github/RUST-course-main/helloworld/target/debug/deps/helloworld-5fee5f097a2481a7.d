3cec4359c6e452132a9624f0416bf57c19fbae08
Eggy <abdelfregel4@gmail.com>
2022-07-15 17:19:47 -0400

    Guess number

---
+++ Github/RUST-course-main/helloworld/target/debug/deps/helloworld-5fee5f097a2481a7.d
@@ -0,0 +1,5 @@
+/home/eggy/disco/Study/Rust/Project/Github/RUST-course-main/helloworld/target/debug/deps/helloworld-5fee5f097a2481a7: src/main.rs
+
+/home/eggy/disco/Study/Rust/Project/Github/RUST-course-main/helloworld/target/debug/deps/helloworld-5fee5f097a2481a7.d: src/main.rs
+
+src/main.rs:
---
+++ Github/RUST-course-main/helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/home/eggy/disco/Study/Rust/Project/Github/RUST-course-main/helloworld/target/debug/helloworld: /home/eggy/disco/Study/Rust/Project/Github/RUST-course-main/helloworld/src/main.rs
